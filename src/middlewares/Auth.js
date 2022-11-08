d3207c75776e353bbfb3fcd8e93ef6f2c74dbe52
GYANESHWER <103898023+Gyaneshwer694@users.noreply.github.com>
2022-11-09 03:03:29 +0530

    Update Auth.js

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -5,7 +5,7 @@ const validateToken = function(req, res, next) {
   
     if (!token) return res.send({ status: false, msg: "token required" });
 
-    let decodedToken = jwt.verify(token, "lithium-rupam");
+    let decodedToken = jwt.verify(token, "lithium-Gyaneshwer");
     if (!decodedToken) {
       return res.send({ status: false, msg: "token is invalid" });
     }
